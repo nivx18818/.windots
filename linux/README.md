be2fd93ea985df657887f987bb9baa50dccbe772
NguyenXuanVinh <nivx87@gmail.com>
2026-08-03 16:07:31 +0700

    feat: add linux setup files
    - add README with dual‑boot and bootstrap instructions.
    
    - add package lists for pacman, paru, external and manual installations.
    
    - add `apps.txt` defining configuration symlinks.
    
    - add `bootstrap.sh` to install packages and set up configs.

---
+++ linux/README.md
@@ -0,0 +1,33 @@
+# CachyOS setup
+
+## Dual-booting with Windows
+
+> [!NOTE]
+>
+> This guide assumes that you already have your Windows installed on your PC and the BIOS mode is UEFI.
+
+> [!INFO]
+>
+> Reference: https://www.youtube.com/watch?v=8WVk2ntV9Mo
+
+Make a room for your CachyOS using Windows's Disk Management.
+
+Go to [CachyOS's Download page](https://cachyos.org/download/), download the Desktop Edition with from Direct source. You will get an ISO file from this step.
+
+Download [Rufus](https://rufus.ie/en/#download) (portable version is supported).
+
+Connect your USB to your PC (make sure to cleanup your USB first).
+
+Open Rufus:
+- Device: Choose your USB.
+- Boot selection, choose the ISO file you've downloaded.
+- Partition scheme: GPT.
+- Target system: UEFI (non CSM).
+- Leave the rest as is, click Start, then spam the OK button.
+- Once done, you're ready to dual-boot.
+
+*..to be continued..*
+
+## Bootstrap
+
+Run `bootstrap.sh` for app installation and config setup.
---
+++ linux/bootstrap.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
+DOTFILES_ROOT="$(realpath "$SCRIPT_DIR/..")"
+
+CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+
+# Other package managers
+sudo pacman -Sy paru flatpak
+
+sudo pacman -S --needed $(grep -vE '^\s*(#|$)' _packages/pacman.txt)
+paru -S --needed $(grep -vE '^\s*(#|$)' _packages/aur.txt)
+
+while read -r cmd; do
+    [[ -z "$cmd" || "$cmd" =~ ^# ]] && continue
+    eval "$cmd"
+done < "$SCRIPT_DIR/_packages/external.txt"
+
+echo
+echo "The following apps cannot be installed automatically:"
+echo "==========================="
+
+while IFS='|' read -r name type url; do
+    [[ -z "$name" || "$name" =~ ^# ]] && continue
+
+    echo
+    echo "$name ($type)"
+    echo "  Visit: $url"
+done < "$SCRIPT_DIR/_packages/manual.txt"
+
+expand_path() {
+    local path="$1"
+    printf '%s\n' "${path/#\~/$HOME}"
+}
+
+while IFS='|' read -r src dst method; do
+    # Skip comments and empty lines
+    [[ -z "${src// }" || "$src" =~ ^[[:space:]]*# ]] && continue
+
+    method="${method:-symlink}"
+
+    src="$DOTFILES_ROOT/$src"
+    
+    if [[ ! -e "$src" ]]; then
+        echo "[ERROR] Source not found: %s\n" "${src#$DOTFILES_ROOT/}" >&2
+        continue
+    fi
+    
+    src="$(realpath "$src")"
+
+    if [[ -z "$dst" ]]; then
+        dst="$CONFIG_HOME/$(basename "$src")"
+    else
+        dst="$(expand_path "$dst")"
+    fi
+
+    mkdir -p "$(dirname "$dst")"
+
+    case "$method" in
+        symlink)
+            ln -sfn "$src" "$dst"
+            DOTFILES_ROOT="/a/b/" \
+            src="/a/b/c" \
+            dst="d" \
+            printf "[LINK]  %-20s -> %s\n" "${src#$DOTFILES_ROOT/}" "$dst"
+            ;;
+
+        copy)
+            rm -rf "$dst"
+            cp -a "$src" "$dst"
+            printf "[COPY]  %-20s -> %s\n" "${src#$DOTFILES_ROOT/}" "$dst"
+            ;;
+
+        *)
+            echo "[ERROR] Unknown method '%s' for '%s'\n" "$method" "${src#$DOTFILES_ROOT/}" 
+            ;;
+    esac
+done < "$SCRIPT_DIR/apps.txt"
