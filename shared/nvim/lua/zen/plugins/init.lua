ca29cb2ed4d323e459c6a8a741a417ca4d6956b1
NguyenXuanVinh <nivx87@gmail.com>
2026-08-07 14:59:10 +0700

    chore(nvim): add `cord.nvim`, `nvim-spider` and `vim-wakatime`

--- shared/nvim/lua/zen/plugins/init.lua
+++ shared/nvim/lua/zen/plugins/init.lua
@@ -164,4 +164,26 @@ return {
       return require "zen.conf.treesitter"
     end,
   },
+
+  {
+    'wakatime/vim-wakatime',
+    lazy = false,
+  },
+
+  {
+    "vyfor/cord.nvim",
+    lazy = false,
+    build = ":Cord update",
+  },
+
+  {
+    "chrisgrieser/nvim-spider",
+    vscode = true,
+    lazy = true,
+    keys = {
+      { "w", function() require('spider').motion('w') end, mode = { "n", "o", "x" } },
+      { "e", function() require('spider').motion('e') end, mode = { "n", "o", "x" } },
+      { "b", function() require('spider').motion('b') end, mode = { "n", "o", "x" } },
+    },
+  }
 }
