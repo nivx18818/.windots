1d0562b02dcfca2166de77de4ddd58a647b797cf
NguyenXuanVinh <nivx87@gmail.com>
2026-08-16 08:16:59 +0700

    chore(zsh): enable grep alias and add rg alias

--- linux/zsh/home/aliases.zsh
+++ linux/zsh/home/aliases.zsh
@@ -17,7 +17,8 @@ compdef eza=ls
 alias cat='bat'
 
 # Core utilities
-# alias grep='rg --color=auto'
+alias grep='rg --color=auto'
+alias rg='rg --color=auto'
 alias diff='diff --color=auto'
 alias df='df -h'
 alias cls="precmd() {precmd() {echo }} && clear"
